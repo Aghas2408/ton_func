1a908740f5fdef23b9dfc6c69f68d231ae5274a1
hexens-aghas <aghasi.seyranyan@hexens.io>
2025-04-15 17:34:35 +0400

    Added wrapper, updated main.fc, new tests, jest and ts configs

--- tasks/test1.ts
+++ tasks/test1.ts
@@ -3,3 +3,5 @@ const raw = "a3935861f79daf59a13d6d182e1640210c02f98e3df18fda74b8f5ab141abf18";
 
 const friendly = new Address(0, Buffer.from(raw, "hex"));
 console.log(friendly);
+
+// output EQCjk1hh952vWaE9bRguFkAhDAL5jj3xj9p0uPWrFBq_GEMS;
